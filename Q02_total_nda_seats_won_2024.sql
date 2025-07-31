219a7995801a8997d8cc9d7b01bd7659da5d8df9
Kottolla Karthikeya Reddy <karthikreddy@Kottollas-MacBook-Air.local>
2025-07-31 14:19:36 +0530

    Add Q02: Total NDA seats won in 2024 Lok Sabha elections

---
+++ Q02_total_nda_seats_won_2024.sql
@@ -0,0 +1,15 @@
+SELECT SUM(Won) AS total_noof_seats_won_by_NDA from partywise_results WHERE Party IN ('Bharatiya Janata Party - BJP', 
+                'Telugu Desam - TDP', 
+				'Janata Dal  (United) - JD(U)',
+                'Shiv Sena - SHS', 
+                'AJSU Party - AJSUP', 
+                'Apna Dal (Soneylal) - ADAL', 
+                'Asom Gana Parishad - AGP',
+                'Hindustani Awam Morcha (Secular) - HAMS', 
+                'Janasena Party - JnP', 
+				'Janata Dal  (Secular) - JD(S)',
+                'Lok Janshakti Party(Ram Vilas) - LJPRV', 
+                'Nationalist Congress Party - NCP',
+                'Rashtriya Lok Dal - RLD', 
+                'Sikkim Krantikari Morcha - SKM'
+);-- Calculate the total number of seats won by NDA alliance parties in the 2024 Indian general elections.
