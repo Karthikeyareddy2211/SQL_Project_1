7f6b840fb744680761dfaa0c05ed0639f43c9530
Kottolla Karthikeya Reddy <karthikreddy@Kottollas-MacBook-Air.local>
2025-07-31 15:02:00 +0530

    Added party_alliance column and updated NDA/INDIA alliance mappings for 2024 election results

---
+++ Q03_update_partywise_alliances.sql
@@ -0,0 +1,36 @@
+ALTER TABLE partywise_results ADD party_alliance VARCHAR(100); -- Step 1: Add a new column 'party_alliance' to store alliance info (NDA or INDIA)
+
+SELECT * FROM partywise_results; -- Step 2: View the current contents of the table
+
+UPDATE partywise_results SET party_alliance = 'NDA' WHERE party IN ('Bharatiya Janata Party - BJP', 
+                'Telugu Desam - TDP', 
+				'Janata Dal  (United) - JD(U)',
+                'Shiv Sena - SHS', 
+                'AJSU Party - AJSUP', 
+                'Apna Dal (Soneylal) - ADAL', 
+                'Asom Gana Parishad - AGP',
+                'Hindustani Awam Morcha (Secular) - HAMS', 
+                'Janasena Party - JnP', 
+				'Janata Dal  (Secular) - JD(S)',
+                'Lok Janshakti Party(Ram Vilas) - LJPRV', 
+                'Nationalist Congress Party - NCP',
+                'Rashtriya Lok Dal - RLD', 
+                'Sikkim Krantikari Morcha - SKM'); -- Step 3: Update alliance to 'NDA' for known NDA member parties in 2024 elections
+                
+                SET SQL_SAFE_UPDATES = 0; -- Step 4: Disable safe updates mode for the next update (if using MySQL Workbench)
+                
+UPDATE partywise_results SET party_alliance = 'INDIA' WHERE party NOT IN ('Bharatiya Janata Party - BJP', 
+                'Telugu Desam - TDP', 
+				'Janata Dal  (United) - JD(U)',
+                'Shiv Sena - SHS', 
+                'AJSU Party - AJSUP', 
+                'Apna Dal (Soneylal) - ADAL', 
+                'Asom Gana Parishad - AGP',
+                'Hindustani Awam Morcha (Secular) - HAMS', 
+                'Janasena Party - JnP', 
+				'Janata Dal  (Secular) - JD(S)',
+                'Lok Janshakti Party(Ram Vilas) - LJPRV', 
+                'Nationalist Congress Party - NCP',
+                'Rashtriya Lok Dal - RLD', 
+                'Sikkim Krantikari Morcha - SKM'); -- Step 5: Assign remaining parties to 'INDIA' alliance
+                                
