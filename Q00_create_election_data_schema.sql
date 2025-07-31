077c71fc0db3bb2486ba27de0e952b9729edb579
Kottolla Karthikeya Reddy <karthikreddy@Kottollas-MacBook-Air.local>
2025-07-31 10:27:55 +0530

    Initial commit with schema SQL file

---
+++ Q00_create_election_data_schema.sql
@@ -0,0 +1,8 @@
+create database election_data;
+use election_data;
+create table constituencywise_details( S_No INT, Candidate VARCHAR(100), Party VARCHAR(100), EVM_Votes INT, Postal_Votes INT, Total_Votes INT, Per_of_Votes FLOAT,Constituency_ID INT);
+select*from constituencywise_details;
+select * from constituencywise_results;
+select * from partywise_results;
+select * from states;
+select * from statewise_results;
---
+++ README.md
@@ -0,0 +1,1 @@
+# SQL Election Project
